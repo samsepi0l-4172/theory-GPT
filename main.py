0172a578580df08741414ed02a0836a148a15a41
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2024-07-26 13:17:02 +0900

    chore: Update langchain module import statements

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+
+import streamlit as st
+
+folder = "pages"
+files = ["DocumentGPT", "PrivateGPT", "QuizGPT", "SiteGPT", "MeetingGPT", "InvestorGPT"]
+
+# Create files in pages folder
+os.makedirs(folder, exist_ok=True)
+
+for idx, filename in enumerate(files, 1):
+    file = os.path.join(folder, f"{str(idx).zfill(2)}_{filename}.py")
+    if os.path.exists(file):
+        continue
+    with open(file, "w") as f:
+        f.write(f"import streamlit as st\n\nst.title('{filename}')")
+
+
+st.set_page_config(page_title="FullstackGPT Home", page_icon="🪄")
+
+st.title("FullstackGPT Home")
+
+st.markdown(
+    """
+- [x] [DocumentGPT](/DocumentGPT)
+- [x] [PrivateGPT](/PrivateGPT)
+- [x] [QuizGPT](/QuizGPT)
+- [x] [SiteGPT](/SiteGPT)
+- [x] [MeetingGPT](/MeetingGPT)
+- [x] [InvestorGPT](/InvestorGPT)
+"""
+)
